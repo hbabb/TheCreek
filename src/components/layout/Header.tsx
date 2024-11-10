b51c286a6ea82a679abd47fcc6d8821e09c7133d
Heath Babb <hbabb@hbc-data.com>
2024-11-10 07:34:23 -0500

    chore: fixing eslint errors

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -36,7 +36,7 @@ export function Header() {
                     <div className="flex items-center gap-2">
                     
                     <NavButton
-                        icon={ChurchIcon}
+                        icon={Send}
                         label="Contact"
                         href="/contact"
                     />
